f9652be2bbd40d162a32a2c3eb66bdfe87c1f5c2
Austin Arrell <austinarrell@gmail.com>
2021-09-14 21:48:04 -0700

    Some basic structure and planning

---
+++ planning/commands.md
@@ -0,0 +1,15 @@
+
+# **Travel**
+* walk
+* look
+
+# **Interaction**
+* get
+* put
+* use
+
+# Communication
+* emote \<message>
+* wave \<mob>
+* poke \<mob>
+* narrate \<message>
---
+++ source/main.cpp
@@ -0,0 +1,15 @@
+//aliasing
+// command option option option
+
+// walk north quietly
+// sneak
+
+// take all items | open chest
+
+// put <item> <container>
+
+int main()
+{
+
+    return 0;
+}
